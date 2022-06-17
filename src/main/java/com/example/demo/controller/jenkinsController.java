74ab7892ee6049669efd9c26199b989171d63e39
bpravat <bpravat@agsft.com>
2022-06-17 19:16:32 +0530

    First Commit jenkinApplication

---
+++ src/main/java/com/example/demo/controller/jenkinsController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class jenkinsController {
+	
+	@GetMapping("/view")
+	public String view() {
+		
+		return "WEL COME TO JENKINS APPLICATION";
+	}
+
+}
